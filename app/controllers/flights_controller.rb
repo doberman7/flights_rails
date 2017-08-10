9aa0c6248e6571528b05de8112a176d8f9420f15
doberman7 <ivanrubioangeles@gmail.com>
2017-08-10 18:16:51 -0500

    vista show desarrollo

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,40 +6,41 @@ class FlightsController < ApplicationController
     p "-" * 50
     origen = params[:flow][:flight_origin]
     destino = params[:flow][:flight_destiny]
-    p Flight.where(origin: origen, destiny: destino)
+    @flight = Flight.where(origin: origen, destiny: destino)
     p "-" * 50
-    render 'index'
-
-
+    #render 'index'
     #@flight = Flight.find(params[:id])
   end
-  def new
-    @flight = Flight.new
-  end
-  def create
-    @flights = Flight.all
-    @flight = Flight.create(flight_params)
-  end
-  def edit
-    @flight = Flight.find(params[:id])
-  end
 
-  def update
-    @flights = Flight.all
-    @flight = Flight.find(params[:id])
 
-    @flight.update_attributes(flight_params)
-  end
 
-  def delete
-    @flight = Flight.find(params[:flight_id])
-  end
-
-  def destroy
-    @flights = Flight.all
-    @flight = Flight.find(params[:id])
-    @flight.destroy
-  end
+  # def new
+  #   @flight = Flight.new
+  # end
+  # def create
+  #   @flights = Flight.all
+  #   @flight = Flight.create(flight_params)
+  # end
+  # def edit
+  #   @flight = Flight.find(params[:id])
+  # end
+  #
+  # def update
+  #   @flights = Flight.all
+  #   @flight = Flight.find(params[:id])
+  #
+  #   @flight.update_attributes(flight_params)
+  # end
+  #
+  # def delete
+  #   @flight = Flight.find(params[:flight_id])
+  # end
+  #
+  # def destroy
+  #   @flights = Flight.all
+  #   @flight = Flight.find(params[:id])
+  #   @flight.destroy
+  # end
 
   private
     def flight_params
